922472002647f41d20952821506525e45cd7024b
KOZYR001 <Asmorch11122007@yandex.ru>
2026-03-20 18:18:45 +0400

    finish

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,18 @@ import React from 'react'
 import assets from '../assets/assets'
 import ThemeToggleBtn from './ThemeToggleBtn';
 import { useState } from 'react';
+import { motion } from 'motion/react';
 
 const Navbar = ({theme, setTheme}) => {
 
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
-    <div className='flex justify-between items-center px-4 sm:px-12 lg:px-24
+    <motion.div
+    initial={{opacity: 0, y: -50}}
+    animate={{opacity: 1, y: 0}}
+    transition={{duration: 0.6, ease: 'easeOut'}}
+    className='flex justify-between items-center px-4 sm:px-12 lg:px-24
     xl:px-40 py-4 sticky top-0 z-20 backdrop-blur-xl font-medium bg-white/50
     dark:bg-gray-900/70'>
 
@@ -39,7 +44,7 @@ const Navbar = ({theme, setTheme}) => {
         </a>
       </div>
 
-    </div>
+    </motion.div>
   )
 }
 
